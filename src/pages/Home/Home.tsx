4543126452f421583373231dccaf6383212718ca
duadrien <adrien.dulou@ynov.com>
2025-07-05 17:28:35 +0200

    menu bouton + nouveau component bouton + image burger  moins grande

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
 import styles from './Home.module.scss';
+import { Button } from '@/components/ui/Button';
 
 const Home = () => {
   return (
-    <div className={styles.burger_font}>
-      <img 
+    <div className={styles.burger_container}>
+      <div className={styles.burger_font}>
+        <img 
           className={styles.burger_image} 
           src="\assets\images\burger_1.png" 
+          alt="Delicious burger" 
         />
+      </div>
+      <div className={styles.button_overlay}>
+        <Button 
+          variant="tertiary" 
+          size="fat" 
+          onClick={() => console.log('Reserver')}
+        >
+          COMMANDER
+        </Button>
+      </div>
     </div>
   );
 };
