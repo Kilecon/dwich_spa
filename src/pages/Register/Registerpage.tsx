d1665586c5de9fccd9bae584445177254ecf2232
duadrien <adrien.dulou@ynov.com>
2025-10-07 14:40:45 +0200

    début responsif pour le font + début container middle

--- src/pages/Register/Registerpage.tsx
+++ src/pages/Register/Registerpage.tsx
@@ -3,9 +3,12 @@ import styles from './Registerpage.module.scss';
 export const Registerpage = () => {
   return (
     <div className={styles.main_content}>
+      <div className={styles.middleContainer}>
+
+      </div>
         <div className={styles.background}>
             <div className={styles.background2}>
-                
+              
             </div>
         </div>
     </div>
