44b8483e4d1ad82741c214631df1a73bed5fba7f
duadrien <adrien.dulou@ynov.com>
2025-11-17 14:34:21 +0100

    page register finit (responsive) + ajout icone oeil + icon du site responsif suivant dark mode / light mode (bon la page ne marche pas apres il faut du back ...

--- src/pages/Register/Registerpage.tsx
+++ src/pages/Register/Registerpage.tsx
@@ -1,127 +1,120 @@
 import styles from './Registerpage.module.scss';
 import { Button } from '@/components/ui/Button';
+import { useState } from 'react';
 
 export const Registerpage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <div className={styles.main_content}>
+      <div className={styles.creationText}>Créer un compte</div>
       <div className={styles.middleContainer}>
-        <Button 
-          variant="primary" 
-          size="small" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="secondary" 
-          size="small" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="tertiary" 
-          size="small" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <br></br>
-                <Button 
-          variant="primary" 
-          size="medium" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="secondary" 
-          size="medium" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="tertiary" 
-          size="medium" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <br></br>
-                <Button 
-          variant="primary" 
-          size="large" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="secondary" 
-          size="large" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="tertiary" 
-          size="large" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <br></br>
-                <Button 
-          variant="primary" 
-          size="fat" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="secondary" 
-          size="fat" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="tertiary" 
-          size="fat" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <br></br>
-                <Button 
-          variant="primary" 
-          size="squared" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="secondary" 
-          size="squared" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-        <Button 
-          variant="tertiary" 
-          size="squared" 
-          onClick={() => console.log('Reserver')}
-        >
-          TEST
-        </Button>
-      </div>
-        <div className={styles.background}>
-            <div className={styles.background2}>
-            </div>
-        </div>
-    </div>
-    
+        <div className={styles.formContainer}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="prenom">Prénom*</label>
+            <input 
+              type="text" 
+              id="prenom" 
+              placeholder="Tapez ici votre prénom"
+              maxLength={30}
+            />
+          </div>
 
+          <div className={styles.inputGroup}>
+            <label htmlFor="nom">Nom*</label>
+            <input 
+              type="text" 
+              id="nom" 
+              placeholder="Tapez ici votre Nom"
+              maxLength={30}
+            />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="telephone">Téléphone*</label>
+            <input 
+              type="tel" 
+              id="telephone" 
+              placeholder="Tapez ici votre numéro de téléphone"
+              pattern="[0-9]*"
+            />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="email">E-mail*</label>
+            <input 
+              type="email" 
+              id="email" 
+              placeholder="Tapez ici votre E-mail"
+            />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Mot de passe*</label>
+            <div className={styles.passwordContainer}>
+              <input 
+                type={showPassword ? "text" : "password"} 
+                id="password" 
+                placeholder="Tapez ici votre mot de passe (8 caractères minimum)"
+                minLength={8}
+              />
+              <span 
+                className={styles.passwordToggle}
+                onClick={togglePasswordVisibility}
+              >
+                <img 
+                  src="/assets/icons/eye_icon.png" 
+                  className={styles.eye}
+                />
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="confirmPassword">Confirmation mot de passe*</label>
+            <div className={styles.passwordContainer}>
+              <input 
+                type={showConfirmPassword ? "text" : "password"} 
+                id="confirmPassword" 
+                placeholder="Confirmez votre mot de passe"
+                minLength={8}
+              />
+              <span 
+                className={styles.passwordToggle}
+                onClick={toggleConfirmPasswordVisibility}
+              >
+                <img 
+                  src="/assets/icons/eye_icon.png" 
+                  className={styles.eye}
+                />
+              </span>
+            </div>
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <Button variant="primary" size="medium" onClick={() => console.log('creer compte')}>
+              Créer mon compte
+            </Button>
+          </div>
+
+          <div className={styles.loginText}>
+            Déjà client ? &gt;
+          </div>
+        </div>
+      </div>
+      <div className={styles.background}>
+        <div className={styles.background2}></div>
+      </div>
+    </div>
   );
 };
 
